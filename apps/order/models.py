d84de4bfb5d43da6255c01bd36465345fe6cc7c2
Akimzhanov <erasylakimjanov@gmail.com>
2022-11-21 20:56:59 +0600

    order

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,1 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from apps.computer.models import Laptop
+
+
+User = get_user_model()
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('open', 'открыто'),
+        ('in_process', 'в обработке'),
+        ('conceled', 'отмененный'),
+        ('finished', 'завершенный')
+    )
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.RESTRICT,
+        related_name='orders'
+    )
 
